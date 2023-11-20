aae3efd6597960af36e07457d7c27d75f9596de0
Muzdak1 <memuzdak@gmail.com>
2023-11-20 14:13:53 +0500

    Added mutable file

---
+++ muteable.py
@@ -0,0 +1,10 @@
+#  immuteable
+# x = 1
+# print(id(x))
+# x = 2
+# print(id(x))
+
+# muteable
+a = [1,2,3]
+print(type(a))
+
--- variables.py
+++ variables.py
@@ -16,3 +16,10 @@ print(x, y)
 # Assign same value to multiple variabls
 x = y = 1
 print(x)
+# Notes
+
+# Static Vs Dynamic langs
+# Python, JavaScript and Ruby is a dynamic lang, as we don't need to define the type while defining the variables
+# In python every type is a class <class 'int'> all integers are instance of this class
+# C++, C#, Jave is a static lang.
+
