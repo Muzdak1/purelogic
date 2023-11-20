ba7b5148c637bbe0208a7151663e27c3d1f7110a
Muzdak1 <memuzdak@gmail.com>
2023-11-20 12:56:29 +0500

    Added Variable topic file and README file

---
+++ README.md
@@ -0,0 +1,2 @@
+In this repo "purelogic". I will be practicing all the knowledge that I learnt about python.
+You will find every topic must have their separate python file
---
+++ variables.py
@@ -0,0 +1,17 @@
+student_count = 1000
+rating = 4.99
+is_published = False
+name = "Student 1"
+
+description = """
+Multiple 
+Lines
+"""
+
+# Initialize varibale in the same liune
+x, y = 1, 2
+print(x, y)
+
+# Assign same value to multiple variabls
+x = y = 1
+print(x)
